3be35a2a0b802b89825dbbc6dfbc01c2483ccfac
LeoneCarmelo <carmeloleone@outlook.it>
2023-04-26 17:03:21 +0200

    Add Business Section

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script>
 import PageHeader from './components/PageHeader.vue'
+import BusinessSection from './components/BusinessSection.vue'
 export default {
    components: {
-      PageHeader
+      PageHeader,
+      BusinessSection
    },
    data() {
       return {
@@ -13,18 +15,11 @@ export default {
 </script>
 
 <template>
-<PageHeader/>
+   <PageHeader />
    <!-- Main -->
-   <main>
-      <section class="container">
-         <div class="row">
-            <h6 class="text-uppercase pt-5 color_dg">our business areas</h6>
-            <h3 class="fw-bold">Excellence in <span class="bg_lg color_dg px-2 fw-bolder rounded-2">Services</span></h3>
-            <p class="text_grey">We are leaders in providing consultancy services with a set of cutting-edge technologies
-               and a team of experienced and renowned professionals. These are some options that you can hire</p>
-            <a class="text-uppercase bg_dg text-light text-center rounded-2 ms-5">see all</a>
-         </div>
-      </section>
+   <main class="py-4">
+      <BusinessSection/>
+
    </main>
    <!-- Footer -->
    <footer>
@@ -65,20 +60,5 @@ export default {
 main {
    background-color: $bg-business;
 
-   section.container {
-      width: 70%;
-
-      p {
-         width:700px;
-      }
-
-      a {
-         width: 100px;
-         text-decoration: none;
-         height: 50px;
-         line-height: 45px;
-         cursor: pointer;
-      }
-   }
 }
 </style>
---
+++ src/components/BusinessSection.vue
@@ -0,0 +1,177 @@
+<script>
+export default {
+    name: 'BusinessSection'
+
+}
+</script>
+
+<template>
+      <section class="container pb-5">
+         <div class="row">
+            <h6 class="text-uppercase pt-5 color_dg">our business areas</h6>
+            <h3 class="fw-bold">Excellence in <span class="bg_lg color_dg px-2 fw-bolder rounded-2">Services</span></h3>
+            <p class="text_grey">We are leaders in providing consultancy services with a set of cutting-edge technologies
+               and a team of experienced and renowned professionals. These are some options that you can hire</p>
+            <a class="text-uppercase bg_dg text-light text-center rounded-2 ms-5">see all</a>
+         </div>
+         <div class="row row-cols-3 pt-5">
+            <div class="col mb-4">
+               <div class="card p-2 border-0">
+                  <div class="cont_icon d-flex justify-content-between mb-2">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-diagram-2" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                           d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H11a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 5 7h2.5V6A1.5 1.5 0 0 1 6 4.5v-1zM8.5 5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1zM3 11.5A1.5 1.5 0 0 1 4.5 10h1A1.5 1.5 0 0 1 7 11.5v1A1.5 1.5 0 0 1 5.5 14h-1A1.5 1.5 0 0 1 3 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1A1.5 1.5 0 0 1 9 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
+                     </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                           d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                     </svg>
+                  </div>
+                  <h5 class="fw-bold">Audit & Assurance</h5>
+                  <p class="w-100 text_grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+            </div>
+            <div class="col mb-4">
+               <div class="card p-2 border-0">
+                  <div class="cont_icon d-flex justify-content-between mb-2">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-briefcase" viewBox="0 0 16 16">
+                        <path
+                           d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5zm1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0zM1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5z" />
+                     </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                           d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                     </svg>
+                  </div>
+                  <h5 class="fw-bold">Financial Advisory</h5>
+                  <p class="w-100 text_grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+            </div>
+            <div class="col mb-4">
+               <div class="card p-2 border-0">
+                  <div class="cont_icon d-flex justify-content-between mb-2">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-bar-chart" viewBox="0 0 16 16">
+                        <path
+                           d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z" />
+                     </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                           d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                     </svg>
+                  </div>
+                  <h5 class="fw-bold">Analytics and M&A</h5>
+                  <p class="w-100 text_grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+            </div>
+            <div class="col mb-4">
+               <div class="card p-2 border-0">
+                  <div class="cont_icon d-flex justify-content-between mb-2">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-airplane" viewBox="0 0 16 16">
+                        <path
+                           d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Zm.894.448C7.111 2.02 7 2.569 7 3v4a.5.5 0 0 1-.276.447l-5.448 2.724a.5.5 0 0 0-.276.447v.792l5.418-.903a.5.5 0 0 1 .575.41l.5 3a.5.5 0 0 1-.14.437L6.708 15h2.586l-.647-.646a.5.5 0 0 1-.14-.436l.5-3a.5.5 0 0 1 .576-.411L15 11.41v-.792a.5.5 0 0 0-.276-.447L9.276 7.447A.5.5 0 0 1 9 7V3c0-.432-.11-.979-.322-1.401C8.458 1.159 8.213 1 8 1c-.213 0-.458.158-.678.599Z" />
+                     </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                           d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                     </svg>
+                  </div>
+                  <h5 class="fw-bold">Middle Marketing</h5>
+                  <p class="w-100 text_grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+            </div>
+            <div class="col mb-4">
+               <div class="card p-2 border-0">
+                  <div class="cont_icon d-flex justify-content-between mb-2">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-globe-europe-africa" viewBox="0 0 16 16">
+                        <path
+                           d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM3.668 2.501l-.288.646a.847.847 0 0 0 1.479.815l.245-.368a.809.809 0 0 1 1.034-.275.809.809 0 0 0 .724 0l.261-.13a1 1 0 0 1 .775-.05l.984.34c.078.028.16.044.243.054.784.093.855.377.694.801-.155.41-.616.617-1.035.487l-.01-.003C8.274 4.663 7.748 4.5 6 4.5 4.8 4.5 3.5 5.62 3.5 7c0 1.96.826 2.166 1.696 2.382.46.115.935.233 1.304.618.449.467.393 1.181.339 1.877C6.755 12.96 6.674 14 8.5 14c1.75 0 3-3.5 3-4.5 0-.262.208-.468.444-.7.396-.392.87-.86.556-1.8-.097-.291-.396-.568-.641-.756-.174-.133-.207-.396-.052-.551a.333.333 0 0 1 .42-.042l1.085.724c.11.072.255.058.348-.035.15-.15.415-.083.489.117.16.43.445 1.05.849 1.357L15 8A7 7 0 1 1 3.668 2.501Z" />
+                     </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                           d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                     </svg>
+                  </div>
+                  <h5 class="fw-bold">Legal Consulting</h5>
+                  <p class="w-100 text_grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+            </div>
+            <div class="col mb-4">
+               <div class="card p-2 border-0">
+                  <div class="cont_icon d-flex justify-content-between mb-2">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-hdd"
+                        viewBox="0 0 16 16">
+                        <path d="M4.5 11a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zM3 10.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
+                        <path
+                           d="M16 11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V9.51c0-.418.105-.83.305-1.197l2.472-4.531A1.5 1.5 0 0 1 4.094 3h7.812a1.5 1.5 0 0 1 1.317.782l2.472 4.53c.2.368.305.78.305 1.198V11zM3.655 4.26 1.592 8.043C1.724 8.014 1.86 8 2 8h12c.14 0 .276.014.408.042L12.345 4.26a.5.5 0 0 0-.439-.26H4.094a.5.5 0 0 0-.44.26zM1 10v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1z" />
+                     </svg>
+                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                           d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                     </svg>
+                  </div>
+                  <h5 class="fw-bold">Regulatory Risk</h5>
+                  <p class="w-100 text_grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+            </div>
+
+         </div>
+      </section>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+/* #region Utility */
+.color_lg {
+   color: $light-green
+}
+
+.color_dg {
+   color: $dark-green
+}
+
+.bg_lg {
+   background-color: $light-green;
+}
+
+.bg_dg {
+   background-color: $dark-green;
+}
+
+.text_grey {
+   color: $grey-text
+}
+
+/* #endregion Utility */
+section.container {
+      width: 70%;
+
+      p {
+         width: 700px;
+      }
+
+      a {
+         width: 100px;
+         text-decoration: none;
+         height: 50px;
+         line-height: 45px;
+         cursor: pointer;
+      }
+
+      svg {
+         fill: $dark-green;
+         cursor: pointer;
+      }
+   }
+</style>
